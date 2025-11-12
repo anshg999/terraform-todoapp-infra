9da705992344be241a1031174c947cd6237713dd
Ansh Gupta (EXT-Nokia) <ansh.gupta.ext@nokia.com>
2025-11-13 01:22:41 +0530

    cluster1

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -6,14 +6,14 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "dev-rg-back"
-    storage_account_name = "aksstg1"
-    container_name       = "akscontainer"
+    resource_group_name  = "dev-rg-back-ansh"
+    storage_account_name = "aksstg1-anshg"
+    container_name       = "akscontaineransh"
     key                  = "dev.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "3bafb5d4-689a-4313-b617-9f3367263cd7"
+  subscription_id = "b1905f95-a2fa-411c-8afb-c515e3b94481"
 }
